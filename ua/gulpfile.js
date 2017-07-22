984f247e69db6300accbfc49d1218b51d1e1591c
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2017-07-22 21:03:40 +0300

    Compiled vendor package. Updated images

--- ua/gulpfile.js
+++ ua/gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
-
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var merge2 = require('merge2');
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
+var bowerMain = require('bower-main');
+var bowerMainJavaScriptFiles = bowerMain('js','min.js');
+
+gulp.task('vendor', function() {
+  return merge2(
+    gulp.src(bowerMainJavaScriptFiles.minified),
+    gulp.src(bowerMainJavaScriptFiles.minifiedNotFound)
+      .pipe(concat('tmp.min.js'))
+      .pipe(uglify())
+    )
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest('js'))
+});
 
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
@@ -18,6 +33,6 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['vendor', 'sass'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
